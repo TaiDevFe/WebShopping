8f0f0804d04e983ced2ed1a947b40e6224c5b98e
TaiDevFe <lehoanganhtai1115@gmail.com>
2024-11-13 16:11:34 +0700

    Add project files.

---
+++ WebShopping/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopping.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập tên Sản phẩm")]
+		public string Name { get; set; }
+        public string Slug { get; set; }
+		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập mô tả Sản phẩm")]
+		public string Description { get; set; }
+		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập giá Sản phẩm")]
+		public decimal Price { get; set; }
+        public int BrandID { get; set; }
+        public int CategoryId { get; set; }
+        public CategoryModel Category { get; set; }
+        public BrandModel Brand { get; set; }
+    }
+}
---
+++ WebShopping/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using WebShopping.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+//Connection db 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:ConnectedDb"]);
+});
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
