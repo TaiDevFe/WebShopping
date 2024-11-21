1f54a5adebd5bc3c2aa3c2fddf3df3af14e3c250
TaiDevFe <lehoanganhtai1115@gmail.com>
2024-11-21 15:54:06 +0700

    Chức năng thêm sản phẩm

--- WebShopping/Models/ProductModel.cs
+++ WebShopping/Models/ProductModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebShopping.Repository.Validation;
 
 namespace WebShopping.Models
 {
@@ -6,17 +8,24 @@ namespace WebShopping.Models
     {
         [Key]
         public int Id { get; set; }
-		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập tên Sản phẩm")]
+		[Required(ErrorMessage = "Yêu cầu nhập tên Sản phẩm")]
 		public string Name { get; set; }
         public string Slug { get; set; }
 		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập mô tả Sản phẩm")]
 		public string Description { get; set; }
-		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập giá Sản phẩm")]
+		[Required(ErrorMessage = "Yêu cầu nhập giá Sản phẩm")]
+        [Range(0.01, double.MaxValue)]
+        [Column(TypeName = "decimal(8, 1")]
 		public decimal Price { get; set; }
+        [Required, Range(1, int.MaxValue, ErrorMessage = "Chọn một thương hiệu")]
         public int BrandID { get; set; }
+        [Required, Range(1, int.MaxValue, ErrorMessage = "Chọn một danh mục")]
         public int CategoryId { get; set; }
         public CategoryModel Category { get; set; }
         public BrandModel Brand { get; set; }
         public string Image {  get; set; }
+        [NotMapped]
+        [FileExtension]
+        public IFormFile ImageUpload { get; set; }
     }
 }
