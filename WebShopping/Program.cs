1ea7803a03f21f846814d999bca0a47651eba0b6
TaiDevFe <lehoanganhtai1115@gmail.com>
2024-11-23 10:00:51 +0700

    Custom route page index

--- WebShopping/Program.cs
+++ WebShopping/Program.cs
@@ -31,6 +31,16 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "Areas",
     pattern: "{area:exists}/{controller=Product}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "category",
+    pattern: "/category/{Slug?}",
+    defaults: new { controller = "Category", action = "Index" }
+    );
+app.MapControllerRoute(
+    name: "brand",
+    pattern: "/brand/{Slug?}",
+    defaults: new { controller = "Brand", action = "Index" }
+    );
 
 app.MapControllerRoute(
     name: "default",
